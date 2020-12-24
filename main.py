edcbae69891b33e773678fd41d4d8084aed133d9
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2020-12-24 01:01:07 -0800

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import sys
 '''for i in range(20):
   url = baseurl + str(i)
   print(wget.download(url, ('m3u/apollotvshows-'+str(i)+'.m3u')))
-  apollolist = streamClasses.rawStreamList('C:\\user\\loacation\\of\\m3u.m3u-'+str(i)+'.m3u')'''
+  apollolist = streamClasses.rawStreamList('C:\\user\\loacation\\of\\m3u.m3u-'+str(i)+'.m3u')''' #location of m3u VODlist.m3u from dload folder. must include .m3u extension
 
 #print(wget.download(iptmovieurl, ('m3u/iptmovies.m3u'))) #if not downloading comment out this line.
-apollomovies = streamClasses.rawStreamList('C:\\user\\loacation\\of\\m3u.m3u')
+apollomovies = streamClasses.rawStreamList('C:\\user\\loacation\\of\\m3u.m3u') #location of m3u VODlist.m3u from dload folder. must include .m3u extension
 
